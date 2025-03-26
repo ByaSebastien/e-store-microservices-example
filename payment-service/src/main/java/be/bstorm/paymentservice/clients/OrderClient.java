5496d1f5598da725606390e6cd2c17445d32a671
Bya Sébastien <sebastien.bya@bstorm.be>
2025-03-26 10:52:50 +0100

    remove unused import + add @ComponentScan sharedservice to all services

--- payment-service/src/main/java/be/bstorm/paymentservice/clients/OrderClient.java
+++ payment-service/src/main/java/be/bstorm/paymentservice/clients/OrderClient.java
@@ -1,11 +1,8 @@
 package be.bstorm.paymentservice.clients;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-
-import java.util.Map;
 
 @FeignClient(name = "order-service")
 public interface OrderClient {
